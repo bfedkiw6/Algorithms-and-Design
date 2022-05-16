cebc800223b8d1fa7f3988ef27569e2900137e32
Ethan Wang <ethanbwang@gmail.com>
2022-05-16 12:30:07 -0700

    Updated huffman.h
    Added CountFrequency function implementation and Added to Compress
    skeleton

--- huffman.h
+++ huffman.h
@@ -2,31 +2,28 @@
 #define HUFFMAN_H_
 
 #include <array>
-#include <cstddef>
 #include <cctype>
+#include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <vector>
 
 #include "bstream.h"
 #include "pqueue.h"
 
 class HuffmanNode {
  public:
-  explicit HuffmanNode(char ch, size_t freq,
-                       HuffmanNode *left = nullptr,
+  explicit HuffmanNode(char ch, size_t freq, HuffmanNode *left = nullptr,
                        HuffmanNode *right = nullptr)
-      : ch_(ch), freq_(freq), left_(left), right_(right) { }
-
+      : ch_(ch), freq_(freq), left_(left), right_(right) {}
 
   bool IsLeaf() {
     // Node is a leaf if it doesn't have any children
     return left_ == nullptr && right_ == nullptr;
   }
 
-  bool operator < (const HuffmanNode &n) const {
+  bool operator<(const HuffmanNode &n) const {
     // In case of equality, make it deterministic based on character
     if (freq_ == n.freq_)
       return ch_ < n.ch_;
@@ -36,8 +33,8 @@ class HuffmanNode {
 
   size_t freq() { return freq_; }
   size_t data() { return ch_; }
-  HuffmanNode* left() { return left_; }
-  HuffmanNode* right() { return right_; }
+  HuffmanNode *left() { return left_; }
+  HuffmanNode *right() { return right_; }
 
  private:
   char ch_;
@@ -53,31 +50,29 @@ class Huffman {
 
  private:
   // Helper methods...
-  // this needs to be changed, possibly to a vector of pairs?
-  std::vector<int> freq_vector;
-  std::vector<HuffmanNode> vec;
-
-  void CountFrequency(std::ifstream &ifs);
+  static void CountFrequency(std::ifstream &ifs,
+                             std::array<int, 128> &freq_array);
+  static void BuildHuffmanTree(std::array<int, 128> &freq_array,
+                               PQueue<HuffmanNode> &huffman_tree);
 };
 
 // To be completed below
-void Huffman::CountFrequency(std::ifstream &ifs) {
-  std::string line;
+void Huffman::CountFrequency(std::ifstream &ifs,
+                             std::array<int, 128> &freq_array) {
+  char cur_char;
 
-  while(std::getline(ifs, line)) {
-    for(int i = 0; i < line.length(); ++i) {
-      char ch = line[i];
-
-
-    }
-  }
+  while (ifs >> cur_char)
+    freq_array[cur_char]++;
 }
 
-static void Huffman::Compress(std::ifstream &ifs, std::ofstream &ofs) {
-  CountFrequency(ifs);
-  PQueue<HuffmanNode> pq;
+void Huffman::Compress(std::ifstream &ifs, std::ofstream &ofs) {
+  std::array<int, 128> freq_array;
+  PQueue<HuffmanNode> huffman_tree;
+
+  CountFrequency(ifs, freq_array);
+  BuildHuffmanTree(freq_array, huffman_tree);
 }
 
-static void Huffman::Decompress(std::ifstream &ifs, std::ofstream &ofs);
+void Huffman::Decompress(std::ifstream &ifs, std::ofstream &ofs) {}
 
 #endif  // HUFFMAN_H_
